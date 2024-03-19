72c542360ba25af6d8a3198ca727ab5f3e84836b
sonvh <sonvh.w@gmail.com>
2024-03-19 15:33:29 +0700

    .

--- src/redux/features/dataVersion.js
+++ src/redux/features/dataVersion.js
@@ -1,5 +1,5 @@
 export const DATA_VERSION = "19032024";
 export const resetData = () => {
     let dataVersion = localStorage.getItem("data-version");
-    return DATA_VERSION !== dataVersion;
+    return DATA_VERSION !== dataVersion && false;
 };
