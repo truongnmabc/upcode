e08ad0d41d42d3031501a47bd84bd3551690a13a
sonvh <sonvh.w@gmail.com>
2024-03-29 10:53:54 +0700

    data

--- src/redux/features/dataVersion.js
+++ src/redux/features/dataVersion.js
@@ -1,4 +1,4 @@
-export const DATA_VERSION = "19032024";
+export const DATA_VERSION = "29032024";
 export const resetData = () => {
     let dataVersion = localStorage.getItem("data-version");
     return DATA_VERSION !== dataVersion && false;
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,12 +5,13 @@ import { persistStore, persistReducer } from "redux-persist";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import Config from "@/config";
 import createEncryptor from "redux-persist-transform-encrypt";
-// import createIdbStorage from "@piotr-cz/redux-persist-idb-storage";
+import createIdbStorage from "@piotr-cz/redux-persist-idb-storage";
 import defaultStorage from "redux-persist/lib/storage";
 import rootReducer from "./features/rootReducer";
 
 enableMapSet();
 const _isProduction = process.env.NODE_ENV === "production";
+const base = process.env.NEXT_PUBLIC_WORDPRESS_API_URL;
 
 const reducers = (state, action) => {
     if (action.type === HYDRATE) {
@@ -37,12 +38,35 @@ export const makeStore = () => {
             secretKey: Config.SECRET_KEY,
             onError: function (error) {},
         });
+        let idbStorage = defaultStorage;
+        let whitelist = [];
+        if (globalThis.indexedDB) {
+            if (base == "https://easy-prep.org") {
+                idbStorage = createIdbStorage({
+                    name: "abc-elearning",
+                    storeName: "reduxpersist",
+                });
+                whitelist = [
+                    "appInfoReducer",
+                    "cardReducer",
+                    "listGameReducer",
+                    "testReducer",
+                    "topicReducer",
+                    "timeLeftReducer",
+                ]; // các reducer được tự động load dữ liệu khi vào trang
+            } else {
+                idbStorage = createIdbStorage({
+                    name: base,
+                    storeName: "reduxpersist",
+                });
+            }
+        }
         const persistConfig = {
             key: "root",
-            storage: defaultStorage,
+            storage: idbStorage,
             stateReconciler: autoMergeLevel2,
             transform: [encryptor],
-            whitelist: ["appInfoReducer", "cardReducer", "listGameReducer", "testReducer", "topicReducer", "timeLeftReducer"], // các reducer được tự động load dữ liệu khi vào trang
+            whitelist: whitelist,
         };
         const persistedReducer = persistReducer(persistConfig, reducers);
         const store = configureStore({
