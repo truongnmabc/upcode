677a7053fbbc925fd07c848d06a58e59ff6741fb
sonvh <sonvh.w@gmail.com>
2024-07-17 15:07:08 +0700

    deploy

---
+++ client_deploy.sh
@@ -0,0 +1,10 @@
+rm -rf .next
+if [[ -e build.zip ]]; then
+    rm -rf build.zip
+fi
+yarn
+yarn build
+# cần build trước (chú ý sửa file env thành production)
+zip -r build.zip .next
+gcloud compute scp --project="micro-enigma-235001" --zone="us-central1-a" --recurse ./build.zip passemall-home:/home/sonvh_w_gmail_com
+rm -rf build.zip
