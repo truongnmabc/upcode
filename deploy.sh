a5c65b3f7a70304b962afbb395f711fb76cec2f0
sonvh <sonvh.w@gmail.com>
2024-07-25 14:03:50 +0700

    change-ccna

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-# if [[ -e build-easyprep.zip ]]; then
-#     rm -rf build-easyprep.zip
-# fi
-# yarn
-# yarn build
+rm -rf .next
+if [[ -e build-easyprep.zip ]]; then
+    rm -rf build-easyprep.zip
+fi
+yarn
+yarn build
 # cần build trước (chú ý sửa file env thành production)
 zip -r build-easyprep.zip .next
 gcloud compute scp --project="micro-enigma-235001" --zone="us-central1-a" --recurse ./build-easyprep.zip passemall-home:/home/sonvh_w_gmail_com
