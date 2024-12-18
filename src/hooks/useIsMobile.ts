b58a34fd26900baed44a24e9c00a996e33f2f701
ABC-HieuMai <hieumai@abc-elearning.org>
2024-12-18 13:35:18 +0700

    new fix

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,6 +1,6 @@
 "use client";
-import { useMediaQuery } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export const useIsMobile = () => {
-  return useMediaQuery("(max-width:768px)");
+    return useMediaQuery("(max-width:768px)");
 };
