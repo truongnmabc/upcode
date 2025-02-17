6d1397c0415c7c35ed52f5f0de8298c777b0a621
truongnmabc <truongngo@abc-elearning.org>
2025-02-17 10:44:11 +0700

    update_paypal

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -144,13 +144,13 @@ export const ONE_TIME_PRO = "oneTimePro";
 export const SUBSCRIPTION = "subcription";
 export const ONETIME = "onetime";
 
-export const PAYPAL_SUBSCRIPTION_CLIENT_ID = isProduction
+export const PAYPAL_SUBSCRIPTION_CLIENT_ID = false
     ? "AdB2eO_M5-okrwgabqjSMgbxuJGSXuw7tOTXNIPonty8TiHtCTZGjIErVHaBRYhsGQWNYZQjQlq4tJat"
     : "AVyimUfmrrnWOGW7GFSXlYm77H4O-JvvRBSBMqBDNj1_ATxF-hRsccOmXxx8lenoD1SND5UjC-MlY9Jm";
 
 export const PAYPAL_SUBSCRIPTION_KEY = "subcription_key";
 
-export const PAYPAL_CLIENT_ID = isProduction
+export const PAYPAL_CLIENT_ID = false
     ? "AdB2eO_M5-okrwgabqjSMgbxuJGSXuw7tOTXNIPonty8TiHtCTZGjIErVHaBRYhsGQWNYZQjQlq4tJat"
     : "ASZuK4V1rzGFj333OzSTQM_TeNeD7VWkhTCUjoy2y6p7dgbIAoSYTSGaKwMGiGVoaHMxC-Mdb8D3wa3E";
 
